068010c0ed8cb271c895215a035974369eea5659
Jrolisilva <jroliveira.sa@gmail.com>
2021-05-13 16:05:55 -0300

    Iniciando projeto

---
+++ app/models/client.rb
@@ -0,0 +1,8 @@
+class Client < ApplicationRecord
+  validates :external_code, :name, :email, :contact, presence: true
+  validates :external_code, :email, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_many :shipping_addresses, dependent: :destroy
+  has_many :orders, dependent: :destroy
+end
---
+++ app/models/order.rb
@@ -0,0 +1,25 @@
+class Order < ApplicationRecord
+    validates :external_code, :store_id, :subtotal, :shipping_cost, :total, :date_created, presence: true
+    validates :external_code, uniqueness: { case_sensitive: false }
+    validates :subtotal, :shipping_cost, :total, numericality: { greater_than_or_equal_to: 0 }
+    validate :total_sum
+    validate :total_paid
+  
+    belongs_to :customer
+    belongs_to :shipping_address
+    has_many :payments, dependent: :destroy
+    has_many :order_products
+    has_many :products, through: :order_items
+  
+    def total_sum
+      return if total.blank? || subtotal.blank? || shipping_cost.blank?
+      return if total == subtotal + shipping_cost
+  
+    end
+  
+    def total_paid
+      payment_sum = payments.map(&:value).reduce(&:+)
+      return if payment_sum == total
+  
+    end
+end
